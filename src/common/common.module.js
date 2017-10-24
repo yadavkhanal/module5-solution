8e846095fb9a3acc2f8062586b4a3eb9bd0c0bc7
@ykhanal <ykhanal@ztr.com>
2017-10-24 11:22:26 -0400

    set api path as my subdomain

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+.constant('ApiPath', 'https://yadavkhanal-module5-solution.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
