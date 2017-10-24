d1e4528442e006b5774967739b3fb3a7abda8286
@ykhanal <ykhanal@ztr.com>
2017-10-24 11:27:49 -0400

    controllers hook up

--- src/info.controller.js
+++ src/info.controller.js
@@ -1,3 +1,12 @@
 /**
  * 
  */
+(function(){
+	'use strict';
+	angular.module('restaurant')
+	.controller('InfoController', InfoController);
+	InfoController.$inject = ['MenuService'];
+	function ItemsComponentController(MenuService){
+		var $ctrl = this;
+	}
+})();
--- src/singup.controller.js
+++ src/singup.controller.js
@@ -1,3 +1,12 @@
 /**
  * 
  */
+(function(){
+	'use strict';
+	angular.module('restaurant')
+	.controller('SignUpController',  SignUpController);
+	SignUpController.$inject = ['MenuService'];
+	function CategoriesComponentController(MenuService){
+		var $ctrl = this;
+	}
+})();
